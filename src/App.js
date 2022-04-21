f84f959f5d1f5a58021b2f2abf398943e111472b
Krelllian <65556888+Krelllian@users.noreply.github.com>
2022-04-21 13:41:19 +0300

    add burger nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container } from 'react-bootstrap';
+import BurgerNav from './components/BurgerNav/BurgerNav.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <BurgerNav />
+      <Container style={{ paddingLeft: '95px' }}>
 
-      </header>
+
+      </Container>
     </div>
   );
 }
---
+++ src/components/BurgerNav/BurgerNav.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import './BurgerNav.scss'
+import './burger.scss'
+import { Link } from 'react-router-dom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage, faAddressCard } from '@fortawesome/free-solid-svg-icons'
+
+
+const BurgerNav = () => {
+    const [isActive, setIsActive] = useState(false)
+
+    const toggleBurgerActive = (e) => {
+        console.log('toggleBurgerActive')
+        setIsActive(!isActive)
+        console.log('isActive', isActive)
+    }
+
+
+
+    return (
+        <>
+            <div className={'sidebar-nav' + (isActive ? ' active' : '')}>
+                <div onClick={toggleBurgerActive} className={"sidebar-nav__burger burger" + (isActive ? ' active' : '')}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className='sidebar-nav__links'>
+                    <Link className='sidebar-nav__link' to="/"><FontAwesomeIcon style={{ width: '50px', height: '50px' }}
+                        icon={faImage}></FontAwesomeIcon>{isActive ? <p>Gallery</p> : ''}</Link>
+                    <Link className='sidebar-nav__link' to="/about"><FontAwesomeIcon style={{ width: '50px', height: '50px' }}
+                        icon={faAddressCard}></FontAwesomeIcon>{isActive ? <p>Evgeniy Petrov</p> : ''}</Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default BurgerNav
